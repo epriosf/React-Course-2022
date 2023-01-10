b110a171589580dbbc8ec1a48c73ba6387f78505
Erick Rios <paulrios@outlook.es>
2023-01-10 01:12:24 -0500

    Chapter-11 Food Order App Practice using hooks

---
+++ 11-food-order-app/src/App.js
@@ -0,0 +1,30 @@
+import Header from "./Components/Layout/Header";
+import {useState} from "react";
+import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const showCartHandler = () => {
+        setCartIsShown(true);
+    }
+    const hideCartHandler = () => {
+        setCartIsShown(false);
+    }
+    const orderFoodHandler = () => {
+        console.log('Ordering...');
+    }
+    return (
+        <CartProvider>
+            {cartIsShown && <Cart onClose={hideCartHandler} onOrder={orderFoodHandler}/>}
+            <Header onShowCart={showCartHandler}/>
+            <main>
+                <Meals/>
+            </main>
+        </CartProvider>
+    );
+}
+
+export default App;
