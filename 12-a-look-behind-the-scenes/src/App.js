937978f7d595aaa3314e7d5b1100534e5ba2a758
Erick Rios <paulrios@outlook.es>
2023-01-29 22:10:49 -0500

    Chapter-12 useMemo optimization

--- 12-a-look-behind-the-scenes/src/App.js
+++ 12-a-look-behind-the-scenes/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import {useState, useCallback} from "react";
+import {useState, useCallback, useMemo} from "react";
 import Button from "./components/UI/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 
 function App() {
     const [showParagraph, setShowParagraph] = useState(false);
     const [allowToggle, setAllowToggle] = useState(false);
+    const [listTitle, setListTitle] = useState('My list');
 
     console.log('App running');
     const toggleParagraphHandler = useCallback(() => {
@@ -17,14 +19,24 @@ function App() {
     const allowToggleHandler = () => {
         setAllowToggle(true);
     };
-    return (
-        <div className="App">
-            <h1> Hi There!</h1>
-            <DemoOutput show={showParagraph}/>
-            <Button onClick={allowToggleHandler}>Allow toggling</Button>
+    const changeTitleHandler = useCallback(() => {
+        setListTitle('New Title');
+    }, []);
 
-            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
-        </div>
+    const listItems = useMemo(()=> [5, 3, 1, 10, 9], []);
+    return (<>
+            <div className="App">
+                <h1> Hi There!</h1>
+                <DemoOutput show={showParagraph}/>
+                <Button onClick={allowToggleHandler}>Allow toggling</Button>
+                <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+            </div>
+            <div className="App">
+                <h1> UseMemo Optimization</h1>
+                <DemoList title={listTitle} items={listItems}/>
+                <Button onClick={changeTitleHandler}>Change List Title </Button>
+            </div>
+        </>
     );
 }
 
