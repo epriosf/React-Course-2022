8a9cc6eb36fa9f396ab0e865e5461294e8ff04ca
Erick Rios <paulrios@outlook.es>
2023-01-31 17:20:45 -0500

    Chapter-15 Using custom hooks for reuse code in http requests

--- 15-building-custom-react-hooks/src/App.js
+++ 15-building-custom-react-hooks/src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ForwardCounter from "./components/ForwardCounter";
 import BackwardCounter from "./components/BackwardCounter";
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
+import useHttp from "./hooks/use-http";
+
 function App() {
+    const [tasks, setTasks] = useState([]);
+
+    const {isLoading, error, sendRequest: fetchTasks} = useHttp();
+
+    useEffect(() => {
+        const transformedTasks = tasksObj => {
+            const loadedTasks = [];
+            for (const taskKey in tasksObj) {
+                loadedTasks.push({id: taskKey, text: tasksObj[taskKey].text});
+            }
+            setTasks(loadedTasks);
+        };
+
+        fetchTasks(
+            {url: 'https://react-movies-swapi-api-default-rtdb.firebaseio.com/tasks.json'},
+            transformedTasks
+        );
+    }, [fetchTasks]);
+
+    const taskAddHandler = (task) => {
+        setTasks((prevTask) => prevTask.concat(task));
+    }
     return (
         <React.Fragment>
             <ForwardCounter/>
             <BackwardCounter/>
+            <NewTask onAddTask={taskAddHandler}/>
+            <Tasks
+                items={tasks}
+                loading={isLoading}
+                error={error}
+                onFetch={fetchTasks}
+            />
         </React.Fragment>
     );
 }
