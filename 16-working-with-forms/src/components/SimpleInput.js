35da0ef963b1a4a04ae6a16a35c655dc3ccddf69
Erick Rios <paulrios@outlook.es>
2023-02-03 01:41:55 -0500

    Chapter-16 Form validation with a simple input

---
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -0,0 +1,49 @@
+import {useState} from "react";
+
+const SimpleInput = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    let formIsValid = false;
+
+    if (enteredNameIsValid) {
+        formIsValid = true;
+    }
+
+    const nameInputChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    }
+    const nameInputBlurHandler = (event) => {
+        setEnteredNameTouched(true);
+    }
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault();
+        setEnteredNameTouched(true);
+        if (!enteredNameIsValid) {
+            return;
+        }
+        setEnteredName('');
+        setEnteredNameTouched(false);
+    }
+    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className={nameInputClasses}>
+                <label htmlFor='name'>Your Name</label>
+                <input type='text' id='name' value={enteredName} onChange={nameInputChangeHandler}
+                       onBlur={nameInputBlurHandler}
+                />
+                {nameInputIsInvalid && <p className="error-text">Please, enter a valid name</p>}
+            </div>
+            <div className="form-actions">
+                <button disabled={!formIsValid}>Submit</button>
+            </div>
+        </form>
+    );
+};
+
+export default SimpleInput;
