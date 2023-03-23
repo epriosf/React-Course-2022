5635ac7c63f8ad04beae012dd4629e4644267aec
Erick Rios <paulrios@outlook.es>
2023-03-23 17:08:24 -0500

    Chapter-01 React vs VanillaJS simple example

---
+++ 01-getting-started/react-example/src/App.js
@@ -0,0 +1,12 @@
+import Todo from "./components/ToDo";
+
+function App() {
+  return (
+      <div>
+        <h1>My Todos</h1>
+        <Todo text='Learn React' />
+      </div>
+  );
+}
+
+export default App;
